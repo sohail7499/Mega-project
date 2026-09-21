aadd15bb411ae9eeadf4300aad46613aff9795d8
Sohail Khan <skhan863601@gmail.com>
2026-09-21 12:31:20 +0530

    Add Redux Auth Managemnet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>hello world!</h1>
-    </>
-  );
+  const [loading, setloading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch(() => dispatch(logout()))
+      .finally(() => setloading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-col bg-gray-400">
+      <div className="flex-1">
+        <Header />
+        TODO: <Outlet />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
