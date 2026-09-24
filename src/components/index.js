f2a05490365ed30562d57ce7654900cd354d3eff
Sohail Khan <skhan863601@gmail.com>
2026-09-24 20:06:45 +0530

    feat: add header components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import Container from "./container/container";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
